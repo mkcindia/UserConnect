4c1f037173230853858e3c05805bdb767c604192
mkcindia <mkcindiaofficial@gmail.com>
2024-12-16 16:06:01 +0530

    initial setup

---
+++ client/src/app/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main className="w-full max-w-lg">
+        <h1 className="text-6xl font-bold text-center text-gray-900">
+          Welcome to User Connect!
+        </h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
